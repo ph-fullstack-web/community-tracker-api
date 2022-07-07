c9096293d9e03d44b89aff087a1293de2a9c2a00
Johan Joshua Icogo <johanjoshua.Icogo@ph.softvision.global>
2022-07-07 17:33:05 +0800

    CT-285 handle request

--- pkg/people/get_people_by_skills.go
+++ pkg/people/get_people_by_skills.go
@@ -1,11 +1,13 @@
 package people
 
 import (
+	"strings"
+	"strconv"
 	"github.com/gofiber/fiber/v2"
 )
 
 type GetPeopleRequestBody struct {
-	Skills []int `json:"skills"`
+	Skills string `json:"skills"`
 }
 
 type PeopleWithSkill struct {
@@ -21,12 +23,24 @@ type PeopleWithSkillResponseBody struct {
 
 func (h handler) GetPeopleBySkills(c *fiber.Ctx) error {
 	body := GetPeopleRequestBody{
-		Skills: nil,
+		Skills: "",
 	}
 
 	if err := c.BodyParser(&body); err != nil {
 		return fiber.NewError(fiber.StatusBadRequest, err.Error())
 	}
+	var s []string
+	s = strings.Split(body.Skills, ",");
+    var t2 = make([]int, len(s))
+    
+    for idx, i := range s {
+        j, err := strconv.Atoi(i)
+        if err != nil {
+            panic(err)
+        }
+        t2[idx] = j
+    }
+	
 
 	var people []PeopleWithSkill
 
@@ -34,7 +48,7 @@ func (h handler) GetPeopleBySkills(c *fiber.Ctx) error {
 	//subquery between peopleprimaryskills and peopleskills
 	sub := h.DB.Table("peopleprimaryskills").Select("peopleprimaryskills.peopleid, peopleskills.peopleskillsid, peopleskills.peopleskillsdesc").Joins("inner join peopleskills on peopleprimaryskills.peopleskillsid = peopleskills.peopleskillsid")
 
-	if result := h.DB.Table("people").Select("fullname, sub.peopleskillsid, sub.peopleskillsdesc").Joins("inner join (?) as sub on sub.peopleid = people.peopleid", sub).Where("sub.peopleskillsid IN (1,2)").Scan(&people); result.Error != nil {
+	if result := h.DB.Table("people").Select("fullname, sub.peopleskillsid, sub.peopleskillsdesc").Joins("inner join (?) as sub on sub.peopleid = people.peopleid", sub).Where("sub.peopleskillsid IN (?)", t2).Scan(&people); result.Error != nil {
 		return fiber.NewError(fiber.StatusNotFound, result.Error.Error())
 	}
 
