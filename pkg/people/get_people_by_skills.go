503d8b16a7cbd442caf0cf1496515afcb045c711
Johan Joshua Icogo <johanjoshua.Icogo@ph.softvision.global>
2022-07-07 16:33:54 +0800

    CT-285 add logic for response body

--- pkg/people/get_people_by_skills.go
+++ pkg/people/get_people_by_skills.go
@@ -1,7 +1,6 @@
 package people
 
 import (
-	"strconv"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -13,6 +12,11 @@ type PeopleWithSkill struct {
 	Fullname	string	`gorm:"column:fullname"`
 	PeopleSkillsId	int	`gorm:"column:peopleskillsid"`
 	PeopleSkillsDesc	string	`gorm:"column:peopleskillsdesc"`
+}
+
+type PeopleWithSkillResponseBody struct {
+	Fullname	string	`gorm:"column:fullname" json:"full_name"`
+	Skills	[]string `gorm:"column:skills" json:"skills"`
 }
 
 func (h handler) GetPeopleBySkills(c *fiber.Ctx) error {
@@ -34,7 +38,43 @@ func (h handler) GetPeopleBySkills(c *fiber.Ctx) error {
 		return fiber.NewError(fiber.StatusNotFound, result.Error.Error())
 	}
 
+	occurred := map[string]bool{}
+	var currentMember string
+	currentSkills := []string{}
+
+
+	var members []PeopleWithSkillResponseBody
+	for index, p := range people {
+		if occurred[p.Fullname] != true {
+			occurred[p.Fullname] = true
+
+			//push prev member
+			m := PeopleWithSkillResponseBody {
+				Fullname: currentMember,
+				Skills: currentSkills,
+			}
+			//dont allow empty element to be pushed
+			if index != 0 {
+				members = append(members, m);
+			}
+
+			//reset
+			currentMember = p.Fullname
+			currentSkills = nil
+		}
+		currentSkills = append(currentSkills, p.PeopleSkillsDesc)
+
+		//push last element before ending the loop
+		if len(people)-1 == index {
+			m := PeopleWithSkillResponseBody {
+				Fullname: currentMember,
+				Skills: currentSkills,
+			}
+			members = append(members, m);
+		}
+
+	}
 	
 
-	return c.Status(fiber.StatusCreated).JSON(fiber.Map{"status": fiber.StatusCreated, "message": "Success!", "data": &people})
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{"status": fiber.StatusCreated, "message": "Success!", "data": &members})
 }
