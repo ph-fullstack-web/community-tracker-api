c18cde6d44a91a42e44e4ef77ff87d2ace510e81
Johan Joshua Icogo <johanjoshua.Icogo@ph.softvision.global>
2022-07-15 17:22:40 +0800

    CT-25 logic for update a project

--- pkg/projects/create_project.go
+++ pkg/projects/create_project.go
@@ -30,20 +30,40 @@ func (h handler) CreateProject(c *fiber.Ctx) error {
 	}
 
 	project := &models.Project{}
-	uppercaseProjectName := strings.ToUpper(projectRequest.ProjectName)
-	projectSearchErr := h.DB.Where("UPPER(projectdesc) = ?", uppercaseProjectName).First(&project).Error
-	if projectSearchErr != nil && !errors.Is(projectSearchErr, gorm.ErrRecordNotFound) {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"status": fiber.StatusInternalServerError, "error": projectSearchErr.Error()})
+
+	
+	//update a project
+	if projectRequest.ProjectId != 0 {
+		projectSearchErr := h.DB.Where("projectid = ?", projectRequest.ProjectId).First(&project).Error
+		if projectSearchErr != nil && !errors.Is(projectSearchErr, gorm.ErrRecordNotFound) {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"status": fiber.StatusInternalServerError, "error": projectSearchErr.Error()})
+		}
+
+		if project.ProjectId == 0 {
+			return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{"status": fiber.StatusUnprocessableEntity, "error": "Project doesn't exist"})
+		}
+
+		h.DB.Model(&project).Where("projectid = ?", projectRequest.ProjectId).Updates(&projectData)
+		return c.Status(fiber.StatusCreated).JSON(fiber.Map{"status": fiber.StatusCreated, "message": "Updated data!", "data": &projectData})
+	} else {
+		//create a poject
+		uppercaseProjectName := strings.ToUpper(projectRequest.ProjectName)
+		projectSearchErr := h.DB.Where("UPPER(projectdesc) = ?", uppercaseProjectName).First(&project).Error
+		if projectSearchErr != nil && !errors.Is(projectSearchErr, gorm.ErrRecordNotFound) {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"status": fiber.StatusInternalServerError, "error": projectSearchErr.Error()})
+		}
+
+		//throws error when there is an existing project
+		if project.ProjectId != 0 {
+			return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{"status": fiber.StatusUnprocessableEntity, "error": "Project already exists."})
+		}
+
+		projectResult := h.DB.Create(&projectData)
+		if projectResult.Error != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"status": fiber.StatusInternalServerError, "error": projectResult.Error.Error()})
+		}
+
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{"status": fiber.StatusOK, "message": "Success!", "data": &projectRequest})
 	}
 
-	if project.ProjectId != 0 {
-		return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{"status": fiber.StatusUnprocessableEntity, "error": "Project already exists."})
-	}
-
-	projectResult := h.DB.Create(&projectData)
-	if projectResult.Error != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"status": fiber.StatusInternalServerError, "error": projectResult.Error.Error()})
-	}
-
-	return c.Status(fiber.StatusOK).JSON(fiber.Map{"status": fiber.StatusOK, "message": "Success!", "data": &projectRequest})
 }
