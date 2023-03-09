aab7c622bd7c77b40aa8c55e8ac08749b5eb98e4
crystal.tolentino <crystaljoy.tolentino@gmail.com>
2023-03-09 12:29:33 +0800

    update query for community members search

--- pkg/community_members/get_community_members_search.go
+++ pkg/community_members/get_community_members_search.go
@@ -10,28 +10,30 @@ import (
 )
 
 func (h handler) GetCommunityMembersSearch(c *fiber.Ctx) error {
-	var community_data []models.PeopleUnderCommunitySearch
-	var community_count []models.PeopleUnderCommunitySearch
+	var community_data models.CommunityMembersSearch
+	var members_count []models.PeopleUnderCommunitySearch
 	var count int64
-
-	rows, rowsError := strconv.Atoi(strings.TrimSpace(c.Query("rows")))
-	if rowsError != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"status": fiber.StatusInternalServerError, "message": "Server error."})
-	}
-	
-	page, pageError := strconv.Atoi(strings.TrimSpace(c.Query("page")))
-	if pageError != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"status": fiber.StatusInternalServerError, "message": "Server error."})
-	}
 
 	communityId, communityIdError := strconv.Atoi(c.Params("communityId"))
 	if communityIdError != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"status": fiber.StatusInternalServerError, "message": "Server error."})
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"status": fiber.StatusBadRequest, "message": "Path Parameter is invalid"})
 	}
 
-	h.DB.Where(&models.PeopleUnderCommunitySearch{Communityid: communityId, Isactive: true}).Find(&community_count).Count(&count)
-	result := h.DB.Limit(rows).Offset((page - 1) * rows).Where(&models.PeopleUnderCommunitySearch{Communityid: communityId, Isactive: true}).Find(&community_data)
+	var rowsStr = strings.TrimSpace(c.Query("rows"))
+	rows, rowsError := strconv.Atoi(rowsStr)
+	if rowsError != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"status": fiber.StatusBadRequest, "message": "Query Parameter is invalid"})
+	}
+	
+	var pageStr = strings.TrimSpace(c.Query("page"))
+	page, pageError := strconv.Atoi(pageStr)
+	if pageError != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"status": fiber.StatusBadRequest, "message": "Query Parameter is invalid"})
+	}
 
+	h.DB.Where(&models.PeopleUnderCommunitySearch{Communityid: communityId, Isactive: true}).Find(&members_count).Count(&count)
+	result := h.DB.Where(&models.CommunityMembersSearch{CommunityID: communityId}).Preload("Members", func(tx *gorm.DB) *gorm.DB { return tx.Limit(rows).Offset((page - 1) * rows).Where(models.PeopleUnderCommunitySearch{Isactive: true})}).Preload("Manager").First(&community_data)
+	
 	// show 404 error if no community found
 	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
 		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"status": fiber.StatusNotFound, "message": "Community does not exist."})
